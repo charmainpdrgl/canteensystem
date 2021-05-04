0604c1134fc933f189764c58099301fb7a34abb3
mendokusan <41606810+mendokusan@users.noreply.github.com>
2021-05-04 12:12:13 +1200

    working on sign up.

--- CanteenApp/canteen-wrouter/src/views/About.vue
+++ CanteenApp/canteen-wrouter/src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="q-pa-md">
+
+    <div class="row">
+      <div class="col">
+        .col
+      </div>
+      <div class="col">
+        .col
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        .col
+      </div>
+      <div class="col">
+        .col
+      </div>
+      <div class="col">
+        .col
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="sass" scoped>
+.row > div
+  padding: 10px 15px
+  background: rgba(86,61,124,.15)
+  border: 1px solid rgba(86,61,124,.2)
+.row + .row
+  margin-top: 1rem
+</style>
