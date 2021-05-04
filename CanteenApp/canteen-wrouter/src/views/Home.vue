44e3dce1b73f915004a53cc0ec4a0f05faa01ebb
mendokusan <41606810+mendokusan@users.noreply.github.com>
2021-05-04 12:34:55 +1200

    added styling and homepage into the project with the router.

--- CanteenApp/canteen-wrouter/src/views/Home.vue
+++ CanteenApp/canteen-wrouter/src/views/Home.vue
@@ -1,7 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div>
+        <p>{{ msg }}</p>
+        <div class="hero">
+            <div class="row">
+                <div class="col align-self-center">
+                    <img src="/img/slogan.png" class="hero-image img-fluid" alt="St Hilda's Canteen hero image">
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="grid-container-two">
+            <div class="grid-item">
+                <button class="homebuttons">TOP UP</button>
+            </div>
+            <div class="grid-item">
+                <button class="homebuttons">ORDER</button>
+            </div>
+        </div>
+        <br>
+        <div>
+            <h2>Pre-order and pay for your canteen food online so you can enjoy your break times with friends!</h2>
+        </div>
+        <br>
+        <div class="grid-container-three">
+            <div class="grid-item">
+                <img src="/img/phone.png" alt="You Order" class="homeimages">
+                <br>
+                <h3>YOU ORDER</h3>
+            </div>
+            <div class="grid-item">
+                <img src="/img/shopping-bag.png" alt="We Prepare" class="homeimages">
+                <br>
+                <h3>WE PREPARE</h3>
+            </div>
+            <div class="grid-item">
+                <img src="/img/cutlery.png" alt="You Enjoy" class="homeimages">
+                <br>
+                <h3>YOU ENJOY</h3>
+            </div>
+        </div>
+        <p>Include footer here</p>
+    </div>
   </div>
 </template>
 
@@ -16,3 +56,6 @@ export default {
   }
 }
 </script>
+
+<style scoped src="../assets/style.css">
+</style>
