14da55bc49437f32e9e8078cdb4aa94c03608a77
charmainpdrgl <charmainpedrigal@gmail.com>
2021-05-25 15:52:44 +1200

    deleted new-canteen-app folder because moved contents to canteen-wrouter

--- CanteenApp/new-canteen-app/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "../src/assets/bootstrap.css";
-createApp(App)
-  .use(router)
-  .mount("#app");
