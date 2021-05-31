c93d5927debb7ee6624b3cf53e797ebecac76036
mendokusan <41606810+mendokusan@users.noreply.github.com>
2021-05-31 14:07:41 +1200

    working on top up

--- CanteenApp/canteen-wrouter/src/App.vue
+++ CanteenApp/canteen-wrouter/src/App.vue
@@ -10,7 +10,7 @@
 body,html,#app{
   width: 100%;
   height: 100%;
-  background-color: #F0F1F0
+  background-color: #ffffff
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ CanteenApp/canteen-wrouter/src/views/TopUp.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        1 of 3
+      </div>
+      <div class="col-6">
+        <h2>Top Up</h2>
+        <h3>any ST Hilds Student Account</h3>
+      </div>
+      <div class="col">
+        3 of 3
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        1 of 3
+      </div>
+
+      <div class="col-6">
+        <form>
+          <div class="mb-3">
+            <div class="row">
+              <div class="col-6">
+                <label for="exampleInputName" class="form-label"
+                  >First Name</label
+                >
+                <input type="text" class="form-control" id="exampleInputName" />
+              </div>
+              <div class="col-6">
+                <label for="exampleInputName" class="form-label"
+                  >First Name</label
+                >
+                <input type="text" class="form-control" id="exampleInputName" />
+              </div>
+            </div>
+                          <label for="exampleInputName" class="form-label"
+                >Student E-mail</label
+              >
+              <input type="text" class="form-control" id="exampleInputName" />
+
+          </div>
+        </form>
+      </div>
+      <div class="col">
+        3 of 3
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  padding: 20px;
+}
+.form-panel {
+  margin-top: 20px;
+}
+.form-label {
+  font-size: 1.4em !important;
+}
+h2 {
+  text-align: center;
+}
+label {
+  color: #34abd6;
+}
+input {
+  border: 2px solid #34abd6;
+  background: #f0f1f0;
+}
+.create-button {
+  border: 2px solid #34abd6;
+  font-size: 16px;
+}
+.last-input {
+  margin-bottom: 20px;
+}
+</style>
