139de2ceab8a58e6ac47880e58efb442ca3b921e
mendokusan <41606810+mendokusan@users.noreply.github.com>
2021-06-02 16:53:20 +1200

    Made top up

--- CanteenApp/canteen-wrouter/src/main.js
+++ CanteenApp/canteen-wrouter/src/main.js
@@ -2,4 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '../src/assets/bootstrap.css';
+
 createApp(App).use(router).mount('#app')
--- CanteenApp/canteen-wrouter/src/views/TopUp.vue
+++ CanteenApp/canteen-wrouter/src/views/TopUp.vue
@@ -1,55 +1,86 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        1 of 3
-      </div>
-      <div class="col-6">
-        <h2>Top Up</h2>
-        <h3>any ST Hilds Student Account</h3>
-      </div>
-      <div class="col">
-        3 of 3
+  <!-- npm install vue-slider-component for slider -->
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col"></div>
+        <div class="col-8 justify-content-center">
+          <header>
+            <h2 class="self-align-center">Top Up</h2>
+            </header>
+        </div>
+        <div class="col"></div>
       </div>
     </div>
-  </div>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        1 of 3
-      </div>
-
-      <div class="col-6">
-        <form>
-          <div class="mb-3">
-            <div class="row">
-              <div class="col-6">
-                <label for="exampleInputName" class="form-label"
-                  >First Name</label
-                >
-                <input type="text" class="form-control" id="exampleInputName" />
+    <div class="container">
+      <div class="row">
+        <div class="col"></div>
+        <div class="col-8">
+          <form>
+            <div class="mb-3">
+              <div class="row">
+                <div class="col-6">
+                  <label for="exampleInputName" class="form-label"
+                    >First Name</label
+                  >
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputName"
+                  />
+                </div>
+                <div class="col-6">
+                  <label for="exampleInputName" class="form-label"
+                    >Last Name</label
+                  >
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputName"
+                  />
+                </div>
               </div>
-              <div class="col-6">
-                <label for="exampleInputName" class="form-label"
-                  >First Name</label
-                >
-                <input type="text" class="form-control" id="exampleInputName" />
-              </div>
-            </div>
-                          <label for="exampleInputName" class="form-label"
+              <label for="exampleInputName" class="form-label"
                 >Student E-mail</label
               >
               <input type="text" class="form-control" id="exampleInputName" />
-
-          </div>
-        </form>
-      </div>
-      <div class="col">
-        3 of 3
+            </div>
+            <br />
+            <vue-slider :value="'' + value" @change="(val) => (value = +val)" />
+            <div class="text-value-slide">Selected Amount:${{ value }}</div>
+            <div classs="container">
+              <div class="row">
+                <div class="col-6">
+                  <button class="checkout-button align-content-right">POLI</button>
+                </div>
+                <div class="col-6">
+                  <button class="checkout-button align-content-left">CREDIT</button>
+                </div>
+            </div>
+            </div>
+            <button class="top-up-button">PROCESS TOP UP</button>
+          </form>
+        </div>
+        <div class="col"></div>
       </div>
     </div>
   </div>
 </template>
+<script>
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/default.css";
+
+export default {
+  components: {
+    VueSlider,
+  },
+  data() {
+    return {
+      value: 0,
+    };
+  },
+};
+</script>
 
 <style scoped>
 .title {
@@ -71,11 +102,31 @@ input {
   border: 2px solid #34abd6;
   background: #f0f1f0;
 }
-.create-button {
+.top-up-button {
   border: 2px solid #34abd6;
   font-size: 16px;
+  margin-top:20px;
+  color: #34abd6;
 }
 .last-input {
   margin-bottom: 20px;
 }
+.text-value-slide {
+  color: #34abd6;
+  font-weight: 700;
+}
+.checkout-button {
+  border: none;
+  color: white;
+  padding: 15px 150px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  background-color: #34abd6;
+}
+
+
 </style>
